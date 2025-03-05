54de7fd898c197abc2fae38a844b1d91c433a35e
4805091 <4805091@TE-GDA-D-D21E17>
2025-03-05 13:06:51 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Podaj pierwszą liczbę: ");
+string buffer = Console.ReadLine() ?? "0";
+double a = double.Parse(buffer);
+Console.WriteLine("Podaj drugą liczbę: ");
+buffer = Console.ReadLine() ?? "0";
+double b = double.Parse(buffer);
+double result = a + b;
+Console.WriteLine("Wynik dodawania liczb " + a + " i " + b + " wynosi: " + result);
